7aa36e869cb7ea167671e397e710be7693df7829
forfail <business.dafov@gmail.com>
2025-01-01 21:45:47 +0000

    Added CORS to dev&prod origins

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { TaskGetWord } from "./endpoints/taskGetWord";
-import { TaskGetCocktail } from "endpoints/taskGetCoctail";
+import { TaskGetCocktail } from "./endpoints/taskGetCoctail";
 
 // Start a Hono app
 const app = new Hono();
+
+// Add CORS Middleware globally
+app.use(
+	"/*",
+	cors({
+		origin: ["http://localhost:3000", "https://kdafov.com", "http://localhost:5173"],
+		allowMethods: ["GET", "OPTIONS"],
+		allowHeaders: ["Content-Type"],
+	})
+);
 
 // Setup OpenAPI registry
 const openapi = fromHono(app, {
